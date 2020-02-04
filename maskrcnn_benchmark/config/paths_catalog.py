dd6a0152ba237ebefa0fcede2557a5aa4a8e7f82
Zeyu-yulab <zeyu.lu@u.nus.edu>
2020-02-04 15:17:27 +0800

    training with COCO dataset

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -15,6 +15,10 @@ class DatasetCatalog(object):
             "img_dir": "coco/val2017",
             "ann_file": "coco/annotations/instances_val2017.json"
         },
+        "coco_2017_test": {
+            "img_dir": "coco/test2017",
+            "ann_file": "coco/annotations/instances_test2017.json"
+        },
         "coco_2014_train": {
             "img_dir": "coco/train2014",
             "ann_file": "coco/annotations/instances_train2014.json"
